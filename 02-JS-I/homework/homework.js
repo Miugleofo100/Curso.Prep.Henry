2a01e61c5fc14c3ea84e036c90abad7508926b7d
Miguelofo100 <miguelofo27@gmail.com>
2022-02-09 22:23:38 -0500

    final completo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -200,25 +200,26 @@ function agregarSimboloExclamacion(str) {
   return str + '!'
 }
 
-function combinarNombres(nombre, apellido) {
+function combinarNombres(firstName, lastName) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var resultado = nombre + '' + apellido ;
+  var resultado = firstName + ' ' + lastName;
   return resultado;
 }
 
-function obtenerSaludo(nombre) {
+function obtenerSaludo(name) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'hola' + nombre + '!';0
+  var espacio = 'hola' + name + '!';
+  return espacio;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  var obtenerAreaRectangulo = (alto + ancho);
+  var obtenerAreaRectangulo = (alto * ancho);
   return obtenerAreaRectangulo;
 }
 
@@ -226,7 +227,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return lado * 6;
+  return lado * 4;
 }
 
 
@@ -251,14 +252,17 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra > 1) {
-    return "Dato incorrecto" 
+  if(letra.length > 1) {
+    return "Dato incorrecto"; 
   }
-  else if(letra == 'a' , letra == 'e' , letra == 'i' , letra == 'o' , letra == 'u') {
-    return "Es vocal"
+  else if (letra === 'l' || letra === 'p') {
+    return "Dato incorrecto";
+  }
+  else if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    return "Es vocal";
   }
 }
-
+ 
 
 
 // No modificar nada debajo de esta línea
